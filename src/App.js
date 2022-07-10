67f92a2ce0502b3a1c026f70c078b17858d1f149
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2022-07-10 19:49:39 +0430

    add data table and react router to the app

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import './app.css'
 import Home from "./pages/home/Home";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom'
+import UserList from "./pages/userList/UserList";
 function App() {
   return (
-    <div>
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar />
         <div className="others">
-          <Home />
-          
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/users'>
+              <UserList />
+            </Route>
+          </Switch>
         </div>
-        
       </div>
-    </div>
+    </Router>
   );
 }
 
